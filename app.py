15705668a3726711350c81399c3171d7bbf01d27
Tso0811 <savagetso0811@gmail.com>
2024-02-17 00:40:27 +0800

    1.Create Server 2.Routing

---
+++ app.py
@@ -0,0 +1,21 @@
+
+from flask import Flask #載入 flask
+app=Flask(__name__) #建立 Application 物件
+
+@app.route("/")
+def index():
+    return ("this is homepage")
+
+@app.route("/data/<userName>")
+def dataPage(userName):
+    if userName == "Tso":
+        return "Tso is a badass"
+    else :
+        return 'this is data page '+userName
+
+
+ 
+
+app.run(port=8080)   #啟動網站伺服器預設為5000,可透過 port 參數指定埠號
+
+ 
