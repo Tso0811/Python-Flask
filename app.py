f04fbed1c457bf04a287cd5c162324be060f22ce
Tso0811 <savagetso0811@gmail.com>
2024-02-19 21:45:45 +0800

    template

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 from flask import Flask #載入 flask
 from flask import request  
 from flask import redirect
+from flask import render_template
 import json
 app=Flask(
     __name__,
@@ -9,28 +10,9 @@ app=Flask(
     static_url_path="/abc"
     ) #建立 Application 物件
 
-@app.route("/zh/")
-def zh():
-    return json.dumps({
-    "status":"ok",
-    "text"  :"你好"
-},ensure_ascii=False)  #不要用 ascii 編碼處理中文
- 
-
-@app.route("/en/")
-def en():
-    return json.dumps({
-    "status":"ok",
-    "text":"hello"
-}) 
-
 @app.route("/")
 def index():
-    lang=request.headers.get("accept-language")
-    if lang.startswith("zh"):
-        return redirect("/zh/")
-    else:
-        return redirect("/en/")
+    return render_template("templates",name="小明")
        
 app.run(port=8080)   #啟動網站伺服器預設為5000,可透過 port 參數指定埠號
 
