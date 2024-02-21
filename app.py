fa214ad341698ddf91b93b063ea70227b56575b7
Tso0811 <savagetso0811@gmail.com>
2024-02-21 14:23:30 +0800

    使用temolate 匯入HTML檔案

--- app.py
+++ app.py
@@ -4,15 +4,15 @@ from flask import request
 from flask import redirect
 from flask import render_template
 import json
-app=Flask(
-    __name__,
-    static_folder="test",
-    static_url_path="/abc"
-    ) #建立 Application 物件
+app=Flask( __name__,static_folder="public",static_url_path="/") #建立 Application 物件
 
 @app.route("/")
 def index():
-    return render_template("templates",name="小明")
-       
+    return render_template('index.html')    #使用 template 匯入html
+
+@app.route('/page/')
+def page():
+    return render_template('page.html')
+    
 app.run(port=8080)   #啟動網站伺服器預設為5000,可透過 port 參數指定埠號
 
